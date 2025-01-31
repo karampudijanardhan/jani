7ac211d3395669c1e98698b2aa6d383e4f9df31c
Hareesh7036 <madahareesh12@gmail.com>
2025-01-31 16:16:59 +0530

    created seperate components, for boxes created reusable component with configuration, handled responsivness, used flex box properties for container placements

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import './About.css'
 import About from './ecomerce/About';
+import { Header } from './ecomerce/Header';
+import Home from './ecomerce/Home';
+import { Footer } from './ecomerce/Footer';
 
 
 function App() {
   return (
     <div className="App">
-    <About/>
+    <Header />
+    <Home />
+    <Footer/>
     </div>
   );
 }
